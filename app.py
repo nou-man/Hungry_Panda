54c8b4968fad7f2a454c5c47b422e214bbed3742
nou-man <4pa20cs059@pace.edu.in>
2023-01-10 14:34:49 +0530

    major_update_UI

--- app.py
+++ app.py
@@ -2,38 +2,42 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+active_page = ''
 
 @app.route('/')
 def main():
+   active_page = '/'
    return render_template('index.html')
 
 @app.route('/about')
 def about():
+   active_page = 'about'
    return render_template('about.html')
 
 @app.route('/blog')
 def blog():
+   active_page = 'blog'
    return render_template('blog.html')
 
 @app.route('/contact')
 def contact():
+   active_page = 'contact'
    return render_template('contact.html')
 
 @app.route('/menu')
 def menu():
+   active_page = 'menu'
    return render_template('menu.html')
 
 @app.route('/order')
 def order():
+   active_page = 'order'
    return render_template('order.html')
 
 @app.route('/login')
 def login():
+   active_page = 'login'
    return render_template('login.html')
-
-
-
-
 
 
 
